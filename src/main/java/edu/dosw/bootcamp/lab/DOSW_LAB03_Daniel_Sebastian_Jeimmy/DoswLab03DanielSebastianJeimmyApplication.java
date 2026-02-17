823c7e417c5bacf9ef7ac11b02b657820e72130d
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-02-16 21:15:41 -0500

    maven configurado

---
+++ src/main/java/edu/dosw/bootcamp/lab/DOSW_LAB03_Daniel_Sebastian_Jeimmy/DoswLab03DanielSebastianJeimmyApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.bootcamp.lab.DOSW_LAB03_Daniel_Sebastian_Jeimmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswLab03DanielSebastianJeimmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswLab03DanielSebastianJeimmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/bootcamp/lab/DOSW_LAB03_Daniel_Sebastian_Jeimmy/DoswLab03DanielSebastianJeimmyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bootcamp.lab.DOSW_LAB03_Daniel_Sebastian_Jeimmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswLab03DanielSebastianJeimmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
